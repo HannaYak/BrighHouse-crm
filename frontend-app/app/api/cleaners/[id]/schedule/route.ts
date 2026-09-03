ccd68e5bacbc20ab173f784f10ae7550eb7d8398
Hanna Yakubouskaya <125196805+HannaYak@users.noreply.github.com>
2026-09-03 20:22:44 +0200

    Update route.ts

--- frontend-app/app/api/cleaners/[id]/schedule/route.ts
+++ frontend-app/app/api/cleaners/[id]/schedule/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { prisma } from '../../../../lib/prisma';
+import { prisma } from '../../../../../lib/prisma';
 
 // Сохранить базовый шаблон графика клинера или добавить смену
 export async function POST(
